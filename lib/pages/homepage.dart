ee7dbfd4e495a5e96bcc285c847e39db001b0bed
DavidKlant <david@card-klant.de>
2024-09-10 13:43:25 +0200

    Add bubblebackground

--- lib/pages/homepage.dart
+++ lib/pages/homepage.dart
@@ -1,7 +1,10 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:neon_david_cursor/blocs/name_age_bloc/name_age_bloc_bloc.dart';
 import 'package:neon_david_cursor/widgets/animations/fade_in_from_bottom.dart';
+import 'package:neon_david_cursor/widgets/bubble_background.dart';
 import 'package:neon_david_cursor/widgets/name_input_form.dart';
 
 class HomePage extends StatelessWidget {
@@ -13,46 +16,48 @@ class HomePage extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Name Age Predictor'),
       ),
-      body: SizedBox(
-        width: double.infinity,
-        child: Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: BlocBuilder<NameAgeBloc, NameAgeState>(
-            builder: (context, state) {
-              return Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  if (state is InitialState) const NameInputForm(),
-                  if (state is InitialState) const SizedBox(height: 20),
-                  if (state is LoadingState)
-                    const CircularProgressIndicator()
-                  else if (state is ResultState)
-                    Column(
-                      children: [
-                        FadeInFromBottom(
-                          delayMs: 0,
-                          child: Text(
-                            state.result,
-                            style: Theme.of(context).textTheme.titleMedium,
-                            textAlign: TextAlign.center,
+      body: BubbleBackground(
+        child: SizedBox(
+          width: double.infinity,
+          child: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: BlocBuilder<NameAgeBloc, NameAgeState>(
+              builder: (context, state) {
+                return Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    if (state is InitialState) const NameInputForm(),
+                    if (state is InitialState) const SizedBox(height: 20),
+                    if (state is LoadingState)
+                      const CircularProgressIndicator()
+                    else if (state is ResultState)
+                      Column(
+                        children: [
+                          FadeInFromBottom(
+                            delayMs: 0,
+                            child: Text(
+                              state.result,
+                              style: Theme.of(context).textTheme.titleMedium,
+                              textAlign: TextAlign.center,
+                            ),
                           ),
-                        ),
-                        const SizedBox(height: 10),
-                        FadeInFromBottom(
-                          delayMs: 500,
-                          child: ElevatedButton(
-                            onPressed: () {
-                              context.read<NameAgeBloc>().add(ResetEvent());
-                            },
-                            child: const Text('Try Again'),
+                          const SizedBox(height: 10),
+                          FadeInFromBottom(
+                            delayMs: 500,
+                            child: ElevatedButton(
+                              onPressed: () {
+                                context.read<NameAgeBloc>().add(ResetEvent());
+                              },
+                              child: const Text('Try Again'),
+                            ),
                           ),
-                        ),
-                      ],
-                    ),
-                ],
-              );
-            },
+                        ],
+                      ),
+                  ],
+                );
+              },
+            ),
           ),
         ),
       ),
---
+++ lib/widgets/bubble_background.dart
@@ -0,0 +1,80 @@
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+
+class BubbleBackground extends StatelessWidget {
+  final Widget child;
+
+  const BubbleBackground({super.key, required this.child});
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: [
+        Positioned(
+          right: -MediaQuery.of(context).size.width / 2,
+          top: MediaQuery.of(context).size.width * 0.9,
+          child: LayerBlurEffect(
+            child: Opacity(
+              opacity: 0.8,
+              child: Container(
+                  height: MediaQuery.of(context).size.width * 1.25,
+                  width: MediaQuery.of(context).size.width * 1.25,
+                  decoration: const BoxDecoration(
+                    color: Colors.blueAccent,
+                    shape: BoxShape.circle,
+                  )),
+            ),
+          ),
+        ),
+        Positioned(
+          left: -MediaQuery.of(context).size.width / 2,
+          top: MediaQuery.of(context).size.width * 0.4,
+          child: LayerBlurEffect(
+            child: Opacity(
+              opacity: 0.8,
+              child: Container(
+                  height: MediaQuery.of(context).size.width,
+                  width: MediaQuery.of(context).size.width,
+                  decoration: const BoxDecoration(
+                    color: Colors.orangeAccent,
+                    shape: BoxShape.circle,
+                  )),
+            ),
+          ),
+        ),
+        child,
+      ],
+    );
+  }
+}
+
+class LayerBlurEffect extends StatelessWidget {
+  final Widget child;
+  final double sigmaX;
+  final double sigmaY;
+
+  const LayerBlurEffect({
+    super.key,
+    required this.child,
+    this.sigmaX = 100.0,
+    this.sigmaY = 100.0,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: [
+        child,
+        Positioned.fill(
+          child: BackdropFilter(
+            filter: ImageFilter.blur(sigmaX: sigmaX, sigmaY: sigmaY),
+            child: Container(
+              color: Colors.transparent,
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
